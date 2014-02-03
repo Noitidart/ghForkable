86508ab19823b03417b37e178a67ee82719a7166
Noitidart <noitidart@gmail.com>
2014-02-03 15:50:12 -0800

    initial
    --inital
    -boostrap base taken from https://gist.github.com/Noitidart/8673632
    
    *install.rdf
    -set min and max rev to 26.0 as thats what i tested it in
    
    *boostrap.js
    -uncommented break so does not do frames
    
    *inject.js
    -created inject.js (https://gist.github.com/Noitidart/8794639)
    
    *chrome.manifest
    -litho recommended that i should try inserting chrome because i told him
    i couldnt insert resource, so i set it up, injected the js, didnt work
    got security error: Security Error: Content at
    https://gist.github.com/yajd/8790765 may not load or link to
    chrome://ghforkable/content/inject.js.
    -mdn page recommended do contentaccessible=yes
    https://developer.mozilla.org/en-US/docs/Mozilla/Projects/XULRunner/Debugging_XULRunner_applications?redirectlocale=en-US&redirectslug=Debugging_a_XULRunner_Application

---
+++ bootstrap.js
@@ -0,0 +1,210 @@
+const {interfaces: Ci,	utils: Cu} = Components;
+Cu.import('resource://gre/modules/Services.jsm');
+var selfPath;
+const chromePath = 'chrome://ghforkable/content/';
+const locationToMatch = /http.?:\/\/(.*?.)?github\./;
+
+function addDiv(theDoc) {
+	//Cu.reportError('addDiv');
+	if (!theDoc) {
+		Cu.reportError('no theDoc!')
+		//document not provided, it is undefined likely
+		return;
+	}
+	var location = (theDoc.location + '');
+	if (!locationToMatch.test(location)) {
+		return;
+	}
+	//Cu.reportError('theDoc location matches locationToMatch:' + theDoc.location)
+	if (!theDoc instanceof Ci.nsIDOMHTMLDocument) {
+		//not html document, so its likely an xul document
+		Cu.reportError('theDoc is not an HTML document, it is probably XUL, since i chose to add HTML element, i dont want to add to xul elements, so exit');
+		return;
+	}
+	removeDiv(theDoc);
+	var script = theDoc.createElement('script');
+	script.setAttribute('src', selfPath + 'inject.js');
+	script.setAttribute('id', selfPath + 'ghForkable_inject');
+	theDoc.documentElement.appendChild(script);
+}
+
+function removeDiv(theDoc) {
+	//Cu.reportError('removeDiv');
+	if (!theDoc) {
+		Cu.reportError('no theDoc!')
+		//document not provided, it is undefined likely
+		return;
+	}
+	var location = (theDoc.location + '');
+	if (!locationToMatch.test(location)) {
+		return;
+	}
+	//Cu.reportError('theDoc location matches locationToMatch:' + theDoc.location)
+	if (!theDoc instanceof Ci.nsIDOMHTMLDocument) {
+		//not html document, so its likely an xul document
+		Cu.reportError('theDoc is not an HTML document, it is probably XUL, since i chose to add HTML element, i dont want to add to xul elements, so exit');
+		return;
+	}
+	var alreadyThere = theDoc.querySelector('#ghForkable_inject');
+	if (alreadyThere) {
+		alreadyThere.parentNode.removeChild(alreadyThere);
+	}
+}
+
+function listenPageLoad(event) {
+	var win = event.originalTarget.defaultView;
+	var doc = win.document;
+	if (win.frameElement) {
+		//its a frame
+		//Cu.reportError('a frame loaded');
+	}
+	addDiv(doc);
+}
+
+/*start - windowlistener*/
+var windowListener = {
+	//DO NOT EDIT HERE
+	onOpenWindow: function (aXULWindow) {
+		// Wait for the window to finish loading
+		let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+		aDOMWindow.addEventListener("load", function () {
+			aDOMWindow.removeEventListener("load", arguments.callee, false);
+			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
+		}, false);
+	},
+	onCloseWindow: function (aXULWindow) {},
+	onWindowTitleChange: function (aXULWindow, aNewTitle) {},
+	register: function () {
+		// Load into any existing windows
+		let XULWindows = Services.wm.getXULWindowEnumerator(null);
+		while (XULWindows.hasMoreElements()) {
+			let aXULWindow = XULWindows.getNext();
+			let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
+		}
+		// Listen to new windows
+		Services.wm.addListener(windowListener);
+	},
+	unregister: function () {
+		// Unload from any existing windows
+		let XULWindows = Services.wm.getXULWindowEnumerator(null);
+		while (XULWindows.hasMoreElements()) {
+			let aXULWindow = XULWindows.getNext();
+			let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+			windowListener.unloadFromWindow(aDOMWindow, aXULWindow);
+		}
+		//Stop listening so future added windows dont get this attached
+		Services.wm.removeListener(windowListener);
+	},
+	//END - DO NOT EDIT HERE
+	loadIntoWindow: function (aDOMWindow, aXULWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		if (aDOMWindow.gBrowser) {
+			aDOMWindow.gBrowser.addEventListener('DOMContentLoaded', listenPageLoad, false);
+			if (aDOMWindow.gBrowser.tabContainer) {
+				//has tabContainer
+				//start - go through all tabs in this window we just added to
+				var tabs = aDOMWindow.gBrowser.tabContainer.childNodes;
+				for (var i = 0; i < tabs.length; i++) {
+					Cu.reportError('DOING tab: ' + i);
+					var tabBrowser = tabs[i].linkedBrowser;
+					var win = tabBrowser.contentWindow;
+					loadIntoContentWindowAndItsFrames(win);
+				}
+				//end - go through all tabs in this window we just added to
+			} else {
+				//does not have tabContainer
+				var win = aDOMWindow.gBrowser.contentWindow;
+				loadIntoContentWindowAndItsFrames(win);
+			}
+		} else {
+			//window does not have gBrowser
+		}
+	},
+	unloadFromWindow: function (aDOMWindow, aXULWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		if (aDOMWindow.gBrowser) {
+			aDOMWindow.gBrowser.removeEventListener('DOMContentLoaded', listenPageLoad, false);
+			if (aDOMWindow.gBrowser.tabContainer) {
+				//has tabContainer
+				//start - go through all tabs in this window we just added to
+				var tabs = aDOMWindow.gBrowser.tabContainer.childNodes;
+				for (var i = 0; i < tabs.length; i++) {
+					Cu.reportError('DOING tab: ' + i);
+					var tabBrowser = tabs[i].linkedBrowser;
+					var win = tabBrowser.contentWindow;
+					unloadFromContentWindowAndItsFrames(win);
+				}
+				//end - go through all tabs in this window we just added to
+			} else {
+				//does not have tabContainer
+				var win = aDOMWindow.gBrowser.contentWindow;
+				unloadFromContentWindowAndItsFrames(win);
+			}
+		} else {
+			//window does not have gBrowser
+		}
+	}
+};
+/*end - windowlistener*/
+
+function loadIntoContentWindowAndItsFrames(theWin) {
+	var frames = theWin.frames;
+	var winArr = [theWin];
+	for (var j = 0; j < frames.length; j++) {
+		winArr.push(frames[j].window);
+	}
+	Cu.reportError('# of frames in tab: ' + frames.length);
+	for (var j = 0; j < winArr.length; j++) {
+		if (j == 0) {
+			Cu.reportError('**checking win: ' + j + ' location = ' + winArr[j].document.location);
+		} else {
+			Cu.reportError('**checking frame win: ' + j + ' location = ' + winArr[j].document.location);
+		}
+		var doc = winArr[j].document;
+		//START - edit below here
+		addDiv(doc);
+		break; //uncomment this line if you don't want to add to frames
+		//END - edit above here
+	}
+}
+
+function unloadFromContentWindowAndItsFrames(theWin) {
+	var frames = theWin.frames;
+	var winArr = [theWin];
+	for (var j = 0; j < frames.length; j++) {
+		winArr.push(frames[j].window);
+	}
+	Cu.reportError('# of frames in tab: ' + frames.length);
+	for (var j = 0; j < winArr.length; j++) {
+		if (j == 0) {
+			Cu.reportError('**checking win: ' + j + ' location = ' + winArr[j].document.location);
+		} else {
+			Cu.reportError('**checking frame win: ' + j + ' location = ' + winArr[j].document.location);
+		}
+		var doc = winArr[j].document;
+		//START - edit below here
+		removeDiv(doc);
+		break; //uncomment this line if you don't want to remove from frames
+		//END - edit above here
+	}
+}
+
+function startup(aData, aReason) {
+	selfPath = aData.resourceURI.spec;
+	Cu.reportError('selfPath = ' + selfPath);
+	windowListener.register();
+}
+
+function shutdown(aData, aReason) {
+	if (aReason == APP_SHUTDOWN) return;
+	windowListener.unregister();
+}
+
+function install() {}
+
+function uninstall() {}
---
+++ inject.js
@@ -0,0 +1,57 @@
+javascript: (function () {
+	var fork, present, star, button, form, forks, count;
+	$('.pagehead-actions li').each(function () {
+		var action = $(this).find('form').attr('action');
+		if (action) {
+			switch (action.split('/').pop()) {
+			case 'fork':
+				fork = $(this);
+				present = true;
+				break;
+			case 'star':
+				star = this;
+				break;
+			case 'unstar':
+				star = this;
+				break;
+			}
+		}
+	});
+	if (!fork) {
+		if (!star) {
+			//no star on this page so exit. this is not a page that can be starred so likely not forked
+			return;
+		}
+		fork = $(star).clone();
+	}
+	button = fork.find('.minibutton');
+	if (present) {
+		button.focus();
+		return;
+	}
+	form = fork.find('form');
+	form.attr('action', form.attr('action').split('/').map(function (a) {
+		return a == 'star' || a == 'unstar' ? 'fork' : a;
+	}).join('/'));
+	button.html('<span class="octicon octicon-git-branch"></span>Fork');
+	forks = $('.counter').filter(function () {
+		var href = $(this).parent().attr('href');
+		return href ? href.split('/').some(function (a) {
+			return a === 'forks';
+		}) : false;
+	});
+	count = fork.find('.social-count');
+	count.html(forks.text() || 0);
+	count.attr('href', forks.parent().attr('href'));
+	if (count[0].tagName == 'SPAN') {
+		count.addClass('js-social-count'); //if its a span it doesnt have this class
+		count.replaceWith(function () {
+			var attrs = {};
+			$.each(this.attributes, function (index, attr) {
+				attrs[attr.name] = attr.value;
+			});
+			return $('<a>', attrs).append(count.contents().clone(true, true));
+		});
+	}
+	$('.pagehead-actions li:last').parent().append(fork);
+}());
